fb54a339f5e358b535c611cce2448b55272c36d1
Travis Whitaker <pi.boy.travis@gmail.com>
2012-10-17 22:11:13 -0400

    The two-pin approach is working better, but still dropping every several hundred cycles...

--- teensy2.c
+++ teensy2.c
@@ -4,44 +4,41 @@
 
 #include <avr/io.h>
 #include <avr/pgmspace.h>
-//DELAY IS SHIT. USE NOPS.
+#include <avr/delay.h>
 
 void zero()
 {
 	PORTD &= ~(1<<0);
 	asm volatile("nop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop");
-	PORTD &= ~(1<<0);
+	PORTD |= (1<<0);
 	asm volatile("nop\nnop\nnop\nnop\nnop\nnop\nnop\nnop");
 }
 
 void one()
 {
-	PORTD |= (1<<0);
-	asm volatile("nop\nnop\nnop\nnop\nnop\nnop\nnop\nnop");
 	PORTD &= ~(1<<0);
+	asm volatile("nop\nnop\nnop\nnop\nnop\nnop\nnop\nnop");
+	PORTD |= (1<<0);
 	asm volatile("nop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop");
 }
 
 int main()
 {
 	CLKPR = 0x80;
-	CLKPR = 0x01; //8 Mhz, 125 nanoseconds per cycle, 8 cycles per microsecond
-	DDRD |= (1<<6);
-	PORTD |= (1<<6);
-	DDRD |= (1<<0);
-	asm volatile("nop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop");
-	zero();zero();zero();zero();zero();one();zero();one();
-	zero();zero();zero();zero();zero();zero();zero();zero();
-	zero();zero();zero();zero();zero();zero();one();zero();
-	DDRD &= ~(1<<0);
+	CLKPR = 0x01; //8 Mhz, 8 cycles per microsecond
+	DDRD |= (1<<6); //D6 output (light)
+	PORTD |= (1<<6); //D6 high (light)
+	DDRD |= (1<<0); //D0 output (data out)
+	DDRD &= ~(1<<1); //D1 input (data in)
+	asm volatile("nop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop");
 	while(1)
 	{
-		if(PIND & (1<<0));
-		else
+		if(~(PIND & (1<<1)))
 		{
-			DDRD |= (1<<0);
-			zero();zero();zero();zero();zero();zero();zero();zero();zero();zero();zero();zero();zero();zero();zero();zero();zero();zero();zero();zero();zero();zero();zero();zero();zero();zero();zero();zero();zero();zero();zero();zero();
-			DDRD &= ~(1<<0);
+			zero();zero();zero();zero();zero();zero();zero();zero();
+			zero();zero();zero();zero();zero();zero();zero();zero();
+			zero();zero();zero();zero();zero();zero();zero();zero();
+			zero();zero();zero();zero();zero();zero();zero();zero();
 		}
 	}
 	return 0;
